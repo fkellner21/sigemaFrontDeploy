ed9df9b1171ec1a43c7635486b9200208497f541
fkellner21 <kellner.francisco@gmail.com>
2025-11-26 13:48:41 -0300

    deloy configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,4 @@
 export const environment = {
   production: true,
-//apiUrl:'http://localhost:8080'
-  //apiUrl: 'https://sigemagestion-epach2e0a7cmc2dh.canadacentral-01.azurewebsites.net'
-  apiUrl: 'https://sigemabe-d0gke3fdbnfza9et.canadacentral-01.azurewebsites.net'
-   // apiUrl:'https://sigemaapi.online'
-
+  apiUrl: 'http://backend:8080/api'
 };
